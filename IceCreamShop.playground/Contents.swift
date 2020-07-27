8897639d6e1ed2d7f8b60aea3f71df10f8abe914
fritzgt <tuanis@live.com>
2020-07-27 12:08:52 +0300

    completed class

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,5 +1,35 @@
-
 struct Flavor{
     var name: String
     var rating: Int
+}
+
+
+enum Size: Double{
+    case small = 2.99
+    case medium = 3.99
+    case large = 4.99
+}
+
+struct Cone{
+    var flavor: Flavor
+    var topping: String
+    var size: Size
+    
+    func eat(){
+        print("Mmm! I love \(flavor.name)!")
+    }
+}
+
+class IceCreamShop{
+    var flavors: [Flavor]?
+    var toppings: [String]?
+    var size: [Size]?
+    var totalSales: Double?
+    
+    func listTopFlavors(){
+        guard let flavors = flavors else { return }
+        let filtered = flavors.filter {$0.rating > 4}
+        print(filtered)
+    }
+    
 }
